4eac37ce4d772eec8b9704db056d43f85abdd716
Salonety <103331568+Salonety@users.noreply.github.com>
2022-06-21 12:57:05 +0530

    update

--- app/src/main/java/com/example/live_broadcasting/MainActivity.kt
+++ app/src/main/java/com/example/live_broadcasting/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.live_broadcasting
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,21 +9,26 @@ import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var RecyclerView: RecyclerView
+    private lateinit var mList: ArrayList<ItemsViewModel>
+    private lateinit var mDbRef: DatabaseReference
+    private lateinit var adapter: CustomAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mAuth=FirebaseAuth.getInstance()
+        mDbRef= FirebaseDatabase.getInstance().getReference()
 
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
-
-        // this creates a vertical layout Manager
         recyclerview.layoutManager = LinearLayoutManager(this)
-
-        // ArrayList of class ItemsViewModel
         val data = ArrayList<ItemsViewModel>()
+
 
         // This loop will create 20 Views containing
         // the image with the count of view
